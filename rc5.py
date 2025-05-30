749c61ced8f7a97c72ae7ec07f7550d0c4bbd180
Gabriel Ospina Millan <gabriel.ospina01@uceva.edu.co>
2025-05-30 14:26:14 -0500

    hola

--- rc5.py
+++ rc5.py
@@ -8,7 +8,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes, serialization
 import base64
-
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 
 app = FastAPI()
 # Middleware CORS para permitir solicitudes desde tu frontend
@@ -161,12 +162,57 @@ class CesarDecryptRequest(BaseModel):
 
 # ---- ALgoritmo RSA ---- #
 
-# Generador de Keys RSA
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
+# === Claves PEM que tú defines manualmente ===
+PEM_PRIVATE_KEY = b"""-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCy+TqA8oSWOBOk
+BPW1CrsqdV2sOuA5FO6ZBWgAYq5ZNeB+IxQ8gfnFXjg6ZFDT0+ysaJaJ2GPufh7I
+8QavSKi30ThOamEA/g2BGgYxVx+hr10k+BptkL4w9ZfruFABS5mhQxzFcQxOjRTl
+m/zEhbFlHfwi9M9ygC/9eqpCO6aOUxQCE+L+uvySNdxRLMsZsVvBQThmXKNLQ20X
+Nel/5RkgXVGoERUdH4an9Ww5v+UqeDgmAIdlLDhPCsgh3ObblPdIl/DMHu4zmD9E
+wEiGB2uau+9kyrAXZgZvRIFp+VrirbShC4lr2qBr5jwNdPv45t3X7z8jLXxNUyMB
+VmvnDcoVAgMBAAECggEAMePx4BI6zZlSOE29pLsaE6rl8uXyrVbdcECmjjzz9yBi
+tSbYNEnTM6ahK/3c3j/cYO9Cz6hHqyrzL/aoc2JgzeT/ujRDWEB5yTxU75om0i6N
+W4nh7BsGOaWstOQLQWEusVqBAbUSxi7j3WHPY4UVP2fNan0t+vAq8JEqNw5jzpZ4
+zaynjHzBPTJ9wft8WK3GCu4EDtbKrnEaXO/K/BIqd+lohbKGEmhhKSkTbzLx/bmI
+Dr65l4pZQy7OgyzbXY2izSlBd2/kpvYrTzD+haoH8f5UycdagZsdnBycHlM5T/Fv
+/m2AgIN8byUW2HXDJT1GP6T4VFbZdEWv9ieDpA/CHQKBgQDWsFBRELzl5H/q9vN+
+87bBOdI4XbEIX0jFRMrNidMcqTQRclfAP29u5VmZ0rJTSzJ0XrzKpN9Fl+diryCD
+XhagDnt8quddWoqN3t7j77d97/MutJlsIDDHzyd3qdoFxlIc55TebsNGgpq7BkHu
+pMBYHtml0DEBXvWk79qaywJj0wKBgQDVaZC25LnO2X2R4/r2wIKvmC2WqNpUoVmU
+v/1HJo1udWWyLK9fI2jGDtXFQTL3o0a/qzMyjZpiufCap7L6fIpmmhWFwx2yaRuP
+1Q3GhwfPV9MRtXH8lcETTwSqpX5GOhGFmAIN56NFFFW/XQiK8IN2ZORxPkJAu/WK
+y00EVRIxdwKBgQDJB0n9zpWDEyawptM0whMT9geh3iFiPI7QoWzhb7gt80adO5tt
+mjjG952iCwgONLCnBPr1KdMjwGzbHC9us1CuSUFaaFHDOX/Z/qJpf1Mrat8kkdnD
+xJTI1HIZfAN6Os0y4aqlacQSp0Mp818TjlPJcAHPYRYyOW/9JNdm8MdpHwKBgDPb
+jAu1NqVZ2hZ6VIBjNR5gFyvV6f/ImVD4+h2w84sUwdpy20Z9/zapYi6lcjIUndtz
+SWSOXiaBUkLyXx5Y5vSZmoM/b1bsDYN83PUR2Mb8a1CRS5p59Umw2MxynGWKNLnG
+vzELyXO+xjtS6wGAAiHh0FToMQcz5S2NyzAzT9FvAoGAO07wglEticI4+vAg8HD5
+/PcZ5elAkYly2K7xwIwK5q/KrG4dCriJeR0q+j9OqAJZrQ6UUXXDw1xtNesP4Dhg
+t62MyYJFWO/2ekPN+dfgaWsWaAEgyaC3vlw3JDmmAJXUuBhS43iwr2mjZA83As0j
+7qORX1MdkMbe/iR5bwe5T8E=
+-----END PRIVATE KEY-----"""
+
+PEM_PUBLIC_KEY = b"""-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsvk6gPKEljgTpAT1tQq7
+KnVdrDrgORTumQVoAGKuWTXgfiMUPIH5xV44OmRQ09PsrGiWidhj7n4eyPEGr0io
+t9E4TmphAP4NgRoGMVcfoa9dJPgabZC+MPWX67hQAUuZoUMcxXEMTo0U5Zv8xIWx
+ZR38IvTPcoAv/XqqQjumjlMUAhPi/rr8kjXcUSzLGbFbwUE4ZlyjS0NtFzXpf+UZ
+IF1RqBEVHR+Gp/VsOb/lKng4JgCHZSw4TwrIIdzm25T3SJfwzB7uM5g/RMBIhgdr
+mrvvZMqwF2YGb0SBafla4q20oQuJa9qga+Y8DXT7+Obd1+8/Iy18TVMjAVZr5w3K
+FQIDAQAB
+-----END PUBLIC KEY-----"""
+
+# === Cargar claves desde los strings PEM ===
+private_key = serialization.load_pem_private_key(
+    PEM_PRIVATE_KEY,
+    password=None,
+    backend=default_backend()
 )
-public_key = private_key.public_key()
+
+public_key = serialization.load_pem_public_key(
+    PEM_PUBLIC_KEY,
+    backend=default_backend()
+)
 
 # Modelos Pydantic
 class RSAEncryptRequest(BaseModel):
@@ -179,17 +225,16 @@ class RSADecryptRequest(BaseModel):
 @app.post("/encrypt_rsa")
 def encrypt_rsa(data: RSAEncryptRequest):
     encrypted = public_key.encrypt(
-        data.text.encode(), # Aqui se convierte el texto en Bytes, porque RSA no trabaja con cadenas
+        data.text.encode(),
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
         )
     )
-    encrypted_b64 = base64.b64encode(encrypted).decode() # Convierte el resultado de binario a base64 para enviarlo por JSON
+    encrypted_b64 = base64.b64encode(encrypted).decode()
     return {"encrypted_text": encrypted_b64}
 
-# Descifrar RSA
 @app.post("/decrypt_rsa")
 def decrypt_rsa(data: RSADecryptRequest):
     encrypted_bytes = base64.b64decode(data.encrypted_text)
@@ -211,7 +256,7 @@ def get_colum_order(key: str):
     return sorted(range(len(key)), key=lambda k: key[k]) # Ordena las letras de la clave en orden alfabético y retorna los indices
 
 # Cifrar Transposición
-def transposition_encrypt(text: str, key: int):
+def transposition_encrypt(text: str, key: str):
     order = get_colum_order(key)
     num_cols = len(key)
     num_rows = math.ceil(len(text) / num_cols)
